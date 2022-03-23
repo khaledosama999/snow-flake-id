7c6a65d1f91bfd0f10ee23c32a5ef1875f6b2ca0
khaledosama999 <khaledosama52@gmail.com>
2022-03-23 19:04:03 +0200

    Initial

---
+++ rollup.config.js
@@ -0,0 +1,15 @@
+import typescript from '@rollup/plugin-typescript';
+// import uglify from 'rollup-plugin-uglify'
+
+export default {
+  input: 'src/index.ts',
+  plugins: [
+    typescript(),
+    // uglify()
+  ],
+  output: {
+    dir: './build',
+    format: 'umd',
+    name: 'heap',
+  },
+};
